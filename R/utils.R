eb32d4e974e91005055e9f8436221716b18e5681
Kato Akiru <am11.44@gmail.com>
2021-02-18 14:36:16 +0900

    Update pack function

--- R/utils.R
+++ R/utils.R
@@ -39,7 +39,8 @@ isDynAvailable <- function(dynlib = "libmecab") {
 #' Pack Output of POS Tagger
 #'
 #' @param df Output of \code{pos(format = "data.frame")}, \code{posParallel(format = "data.frame")} or \code{posSimple}.
-#' @param collapse This argument will be passed to \code{paste()}.
+#' @param pull Column name to be packed into data.frame. Default value is `token`.
+#' @param .collapse This argument will be passed to \code{paste()}.
 #' @return data.frame
 #'
 #' @examples
@@ -50,10 +51,12 @@ isDynAvailable <- function(dynlib = "libmecab") {
 #' }
 #'
 #' @export
-pack <- function(df, collapse = " ") {
+pack <- function(df, pull = "token", .collapse = " ") {
   res <- df %>%
     dplyr::group_by(doc_id) %>%
-    dplyr::group_map(~ paste(.x$token, collapse = collapse)) %>%
+    dplyr::group_map(
+      ~ dplyr::pull(.x, {{ pull }}) %>%
+        stringr::str_c(collapse = .collapse)) %>%
     purrr::map_dfr(~ data.frame(text = .)) %>%
     tibble::rowid_to_column("doc_id")
   return(res)
